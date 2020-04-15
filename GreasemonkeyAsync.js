2db6e8ffd68d09014301cf2049b87b1d593c9437
memset0 <34177126+memset0@users.noreply.github.com>
2020-04-15 14:09:48 +0800

    feat: initial commit

---
+++ GreasemonkeyAsync.js
@@ -0,0 +1,32 @@
+// GreasemonkeyAsync.js by memset0
+
+class GreasemonkeyAsync {
+	read() {
+		return GM.getValue(this.source, undefined)
+			.then(data => {
+				if (data == 'undefined') {
+					return GM.setValue(this.source, this.serialize(this.defaultValue))
+						.then(() => this.defaultValue)
+				} else {
+					return this.deserialize(data)
+				}
+			})
+	}
+
+	write(data) {
+		return GM.setValue(this.source, this.serialize(data))
+	}
+
+	constructor(
+		source, {
+			defaultValue = {},
+			serialize = JSON.stringify,
+			deserialize = JSON.parse
+		} = {}
+	) {
+		this.source = source
+		this.defaultValue = defaultValue
+		this.serialize = serialize
+		this.deserialize = deserialize
+	}
+}
